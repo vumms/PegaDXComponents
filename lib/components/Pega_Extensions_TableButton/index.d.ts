3ffc1b409e835297e810c63a7a72063699885d98
Siva Vummiti <sivakumar.rao@gmail.com>
2024-11-07 18:51:59 +1100

    Adding new component-initial commit

---
+++ lib/components/Pega_Extensions_TableButton/index.d.ts
@@ -0,0 +1,9 @@
+type TableButtonProps = {
+    label: string;
+    value: string;
+    localAction: string;
+    getPConnect: any;
+};
+declare const _default: (props: TableButtonProps) => JSX.Element;
+export default _default;
+//# sourceMappingURL=index.d.ts.map
