36028ec993084d5cc6116817291c61980a2b9f43
Siva Vummiti <sivakumar.rao@gmail.com>
2024-11-13 22:03:50 +1100

    select property added for checkbox selection state

--- lib/components/Pega_Extensions_MUITable/styles.d.ts
+++ lib/components/Pega_Extensions_MUITable/styles.d.ts
@@ -1,2 +1,4 @@
 export declare const StyledGridContainer: import("styled-components").StyledComponent<"div", import("styled-components").DefaultTheme, {}, never>;
+export declare const StyledBox: import("styled-components").StyledComponent<"div", import("styled-components").DefaultTheme, {}, never>;
+export declare const StyledWrapper: import("styled-components").StyledComponent<"div", import("styled-components").DefaultTheme, {}, never>;
 //# sourceMappingURL=styles.d.ts.map
