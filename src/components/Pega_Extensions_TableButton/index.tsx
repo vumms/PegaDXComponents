5f66e06fd24b009ae2da4dd00aad9e9d360554dc
Siva Vummiti <sivakumar.rao@gmail.com>
2024-11-19 14:23:20 +1100

    updated

--- src/components/Pega_Extensions_TableButton/index.tsx
+++ src/components/Pega_Extensions_TableButton/index.tsx
@@ -9,16 +9,17 @@ import StyledPegaExtensionsTableButtonWrapper from './styles';
 type TableButtonProps = {
   label: string;
   value: string;
+  amount: string;
   localAction: string;
   getPConnect: any;
 };
 
 function PegaExtensionsTableButton(props: TableButtonProps) {
-  const { getPConnect, label, value, localAction } = props;  
+  const { getPConnect, label, value, amount, localAction } = props;  
   const launchButtonAction = () => {    
     console.log(getPConnect(), localAction); // This is just to keep the variables active from typescript warnings
     // Row id value variable is populated based on the view metadata that is custom configured on the view 
-    console.log('Row id=', value);
+    console.log('Row id=', value, ' Amount=', amount);
   };
   return (
     <StyledPegaExtensionsTableButtonWrapper>
